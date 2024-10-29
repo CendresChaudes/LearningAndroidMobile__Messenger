e034a255e1a2e7080f855a22b058c9c7fce64dac
CendresChaudes <romqaaa1337@gmail.com>
2024-10-29 18:11:14 +0300

    feat: create base of action bar menu

--- app/src/main/java/com/example/messenger/users/UsersActivity.java
+++ app/src/main/java/com/example/messenger/users/UsersActivity.java
@@ -3,12 +3,14 @@ package com.example.messenger.users;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.messenger.R;
-import com.example.messenger.signUp.SignUpActivity;
+import com.example.messenger.login.LoginActivity;
 
 public class UsersActivity extends AppCompatActivity {
 
@@ -18,8 +20,28 @@ public class UsersActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.users_activitity_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menuItemLogout) {
+            this.launchLoginScreen();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.users_activity);
+    }
+
+    private void launchLoginScreen() {
+        Intent intent = LoginActivity.createIntent(this);
+        startActivity(intent);
     }
 }
