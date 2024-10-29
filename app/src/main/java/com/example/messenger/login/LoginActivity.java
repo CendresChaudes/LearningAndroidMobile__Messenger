17d3c902c861e1c35fb278d7dbab9a1d18b6205f
CendresChaudes <romqaaa1337@gmail.com>
2024-10-29 15:32:28 +0300

    feat: create ui of login screen

---
+++ app/src/main/java/com/example/messenger/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.messenger.login;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.messenger.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
