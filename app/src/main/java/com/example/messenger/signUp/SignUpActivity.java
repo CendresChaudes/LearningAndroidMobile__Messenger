41076beef5d2e601be7fa46a901c3f261c3d946b
CendresChaudes <romqaaa1337@gmail.com>
2024-10-29 15:54:47 +0300

    feat: create ui of sign up screen

---
+++ app/src/main/java/com/example/messenger/signUp/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.messenger.signUp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.messenger.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up_activity);
+    }
+}
