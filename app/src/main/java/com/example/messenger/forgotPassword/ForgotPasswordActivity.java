b1b659430b01916159d0828e4445800a7de510df
CendresChaudes <romqaaa1337@gmail.com>
2024-10-29 15:43:09 +0300

    feat: create ui of forgot password screen

---
+++ app/src/main/java/com/example/messenger/forgotPassword/ForgotPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.example.messenger.forgotPassword;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.messenger.R;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forgot_password_activity);
+    }
+}
