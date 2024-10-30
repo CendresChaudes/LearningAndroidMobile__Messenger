6ea2ec4e1e5ec8022aeaa2efe7726f0ff9f22e7a
CendresChaudes <romqaaa1337@gmail.com>
2024-10-30 11:59:47 +0300

    feat: implement sign up
    WIP: saving user data like name, lastname and age

--- app/src/main/java/com/example/messenger/signUp/SignUpActivity.java
+++ app/src/main/java/com/example/messenger/signUp/SignUpActivity.java
@@ -5,20 +5,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.messenger.R;
-import com.example.messenger.forgotPassword.ForgotPasswordActivity;
-import com.example.messenger.login.LoginActivity;
 import com.example.messenger.users.UsersActivity;
-
-import org.jetbrains.annotations.Contract;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private EditText editTextName;
+    private EditText editTextLastname;
+    private EditText editTextAge;
     private Button buttonSignUp;
+
+    private SignUpViewModel viewModel;
 
     @NonNull
     public static Intent createIntent(Context context) {
@@ -32,27 +40,113 @@ public class SignUpActivity extends AppCompatActivity {
         this.initActivity();
     }
 
-    private void launchLoginScreen() {
-        Intent intent = LoginActivity.createIntent(this);
+    private void launchUsersScreen() {
+        Intent intent = UsersActivity.createIntent(this);
         startActivity(intent);
     }
 
     private void initActivity() {
         this.initViews();
+        this.initViewModel();
 
+        this.observeViewModel();
         this.setOnButtonSignUpClickListener();
     }
 
     private void initViews() {
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextName = findViewById(R.id.editTextName);
+        editTextLastname = findViewById(R.id.editTextLastname);
+        editTextAge = findViewById(R.id.editTextAge);
         buttonSignUp = findViewById(R.id.buttonSignUp);
+    }
+
+    private void initViewModel() {
+        this.viewModel = new ViewModelProvider(this).get(SignUpViewModel.class);
+    }
+
+    private void observeViewModel() {
+        this.viewModel.getErrorMessage().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String message) {
+                if (message != null) {
+                    Toast.makeText(
+                            SignUpActivity.this,
+                            message,
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }
+            }
+        });
+
+        this.viewModel.getUser().observe(this, new Observer<FirebaseUser>() {
+            @Override
+            public void onChanged(FirebaseUser user) {
+                if (user != null) {
+                    launchUsersScreen();
+                    finish();
+                }
+            }
+        });
     }
 
     private void setOnButtonSignUpClickListener() {
         this.buttonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                launchLoginScreen();
+                String email = getTrimmedStringFromEditText(editTextEmail);
+                String password = getTrimmedStringFromEditText(editTextPassword);
+                String name = getTrimmedStringFromEditText(editTextName);
+                String lastname = getTrimmedStringFromEditText(editTextLastname);
+                String age = getTrimmedStringFromEditText(editTextAge);
+
+                boolean isSomeFieldEmpty =
+                        email.isEmpty() ||
+                                password.isEmpty() ||
+                                name.isEmpty() ||
+                                lastname.isEmpty() ||
+                                age.isEmpty();
+
+
+                int transformedAge;
+
+                if (age.isEmpty()) {
+                    transformedAge = 0;
+                } else {
+                    transformedAge = Integer.parseInt(age);
+                }
+
+                boolean isLowerAgeLimitInvalid = transformedAge < 16;
+                boolean isUpperAgeLimitInvalid = transformedAge > 125;
+
+                if (isSomeFieldEmpty) {
+                    Toast.makeText(
+                            SignUpActivity.this,
+                            R.string.fields_cannot_be_empty,
+                            Toast.LENGTH_SHORT
+                    ).show();
+                } else if (isLowerAgeLimitInvalid) {
+                    Toast.makeText(
+                            SignUpActivity.this,
+                            R.string.you_must_be_at_least_16_years_old,
+                            Toast.LENGTH_SHORT
+                    ).show();
+                } else if (isUpperAgeLimitInvalid) {
+                    Toast.makeText(
+                            SignUpActivity.this,
+                            R.string.incorrect_age_specified,
+                            Toast.LENGTH_SHORT
+                    ).show();
+                } else {
+                    viewModel.signUp(email, password, name, lastname, transformedAge);
+                }
             }
         });
+    }
+
+    @NonNull
+    private String getTrimmedStringFromEditText(@NonNull EditText editText) {
+        return editText.getText().toString().trim();
     }
 }
---
+++ app/src/main/java/com/example/messenger/signUp/SignUpViewModel.java
@@ -0,0 +1,66 @@
+package com.example.messenger.signUp;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignUpViewModel extends AndroidViewModel {
+
+    private final FirebaseAuth auth;
+
+    private final MutableLiveData<String> errorMessage;
+    private final MutableLiveData<FirebaseUser> user;
+
+    public SignUpViewModel(@NonNull Application application) {
+        super(application);
+
+        this.auth = FirebaseAuth.getInstance();
+
+        this.errorMessage = new MutableLiveData<>();
+        this.user = new MutableLiveData<>();
+
+        this.setOnAuthStateListener();
+    }
+
+    public void signUp(
+            String email,
+            String password,
+            String name,
+            String lastname,
+            int age
+    ) {
+        this.auth
+                .createUserWithEmailAndPassword(email, password)
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        user.setValue(null);
+                        errorMessage.setValue(e.getMessage());
+                    }
+                });
+    }
+
+    public LiveData<String> getErrorMessage() {
+        return this.errorMessage;
+    }
+
+    public LiveData<FirebaseUser> getUser() {
+        return this.user;
+    }
+
+    private void setOnAuthStateListener() {
+        this.auth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                user.setValue(firebaseAuth.getCurrentUser());
+            }
+        });
+    }
+}
